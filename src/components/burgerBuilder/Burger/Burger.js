55ad433d6335cd1323fa2a3de634ef6e199feccd
Avijit <Avijit009>
2024-01-21 19:09:57 +0600

    added ingredents component

--- src/components/burgerBuilder/Burger/Burger.js
+++ src/components/burgerBuilder/Burger/Burger.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Ingredient from "../ingredient/Ingredient";
+import "./ Burger.css";
 
-const Burger = () => {
-  return <div>Burger</div>;
+const Burger = (props) => {
+  let ingredientArray = props.ingredients.map((item) => {
+    let amountArray = [...Array(item.amount).keys()]; //Creating array here
+    return amountArray.map((_) => {
+      return <Ingredient type={item.type} key={Math.random} />;
+    })
+  })
+  .reduce((arr, element) =>{
+    return arr.concat(element);
+  }, []);
+  return (
+    <div className="Burger">
+      <Ingredient type="bread-top" />
+      {ingredientArray}
+      <Ingredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default Burger;
--- src/components/burgerBuilder/ingredient/Ingredient.js
+++ src/components/burgerBuilder/ingredient/Ingredient.js
@@ -1,7 +1,59 @@
 import React from "react";
+import "./ingredient.css";
 
-const Ingredient = () => {
-  return <div>Ingredient</div>;
+const Ingredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case "bread-top":
+      ingredient = (
+        <div>
+          <img src='assets/images/top.png' alt="Bread Bottom" />
+        </div>
+      );
+      break;
+
+    case "bread-bottom":
+      ingredient = (
+        <div>
+          <img src="assets/images/bottom.png" alt="Bread Top" />
+        </div>
+      );
+      break;
+
+    case "meat":
+      ingredient = (
+        <div>
+          <img src="assets/images/meat.png" alt="Bread Top" />
+        </div>
+      );
+      break;
+
+    case "salad":
+      ingredient = (
+        <div>
+          <img src="assets/images/salad.png" alt="Bread Top" />
+        </div>
+      );
+      break;
+
+    case "cheese":
+      ingredient = (
+        <div>
+          <img src="assets/images/cheese.png" alt="Bread Top" />
+        </div>
+      );
+      break;
+
+    default:
+      ingredient = null;
+  }
+  
+  return (
+    <div className="Ingredient">
+      {ingredient}
+    </div>
+  );
 };
 
 export default Ingredient;
