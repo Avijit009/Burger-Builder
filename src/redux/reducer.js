6c9385e3dc68c417a99f9aa40e5a08fbed9f40d4
Avijit <Avijit009>
2024-01-22 23:19:38 +0600

    Completed order component

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -61,6 +61,25 @@ export const reducer = (state = INITIAL_STATE, action) => {
         totalPrice: 80,
         purchasable: false,
       };
+    case actionTypes.LOAD_ORDERS:
+      let orders = [];
+      for (let key in action.payload) {
+        orders.push({
+          ...action.payload[key],
+          id: key,
+        });
+      }
+      return {
+        ...state,
+        orders: orders,
+        orderLoading: false,
+      };
+    case actionTypes.ORDER_LOAD_FAILED:
+      return {
+        ...state,
+        orderErr: true,
+        orderLoading: false,
+      };
     default:
       return state;
   }
